ca54aaf836ff4cd361106f271e9e64d41ae28ca5
VaMos <example@gmail.com>
2023-05-17 20:48:57 +0200

    pokemons-list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import PokemonList from './components/pokemonList/pokemonList';
 import RandomPokemon from './components/randomPokemon/randomPokemon';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <RandomPokemon/>
+      <PokemonList/>
     </div>
   );
 }
